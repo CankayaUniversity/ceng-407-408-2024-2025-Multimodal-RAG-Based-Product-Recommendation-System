29b473dffcbda2c9eee008be651fab2e9a20e93c
bekX0 <hikmetberkin@gmail.com>
2025-03-27 20:27:27 +0300

    path of keys updated with non-static true path string

--- src/web/backend/auth/auth.py
+++ src/web/backend/auth/auth.py
@@ -3,14 +3,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from functools import wraps
 from datetime import datetime, timezone, timedelta
+from pathlib import Path
 
 auth_bp = Blueprint('auth', __name__)
 
+path_of_keys = Path.joinpath((Path(__file__).parent.parent), "keys")
 
 def load_keys():
-    with open('keys/private.pem', 'r') as f:
+    path_of_private_key = path_of_keys.joinpath("private.pem")
+    path_of_public_key = path_of_keys.joinpath("public.pem")
+    with open(path_of_private_key, 'r') as f:
         private = f.read()
-    with open('keys/public.pem', 'r') as f:
+    with open(path_of_public_key, 'r') as f:
         public = f.read()
     return private, public
 
