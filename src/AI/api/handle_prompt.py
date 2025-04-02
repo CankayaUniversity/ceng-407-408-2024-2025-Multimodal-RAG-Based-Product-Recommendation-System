39556654c2509cb145adb23922c2ae7827bfe5c9
bekX0 <hikmetberkin@gmail.com>
2025-04-02 18:33:36 +0300

    user spesific chat history added to chatbot pipeline

--- src/AI/api/handle_prompt.py
+++ src/AI/api/handle_prompt.py
@@ -1,19 +1,23 @@
-import json
 from flask import jsonify, request
 from . import api_blueprint
-from langchain_methods import rag_pipeline
-from langchain.memory import ConversationBufferMemory
+from langchain_methods import rag_pipeline, get_memory_for_user
 from box import Box
-
-# temp global (further changes require user based)
-memory = ConversationBufferMemory(memory_key="chat_history", input_key="query_text", return_messages=True)
 
 @api_blueprint.route("/handle_prompt", methods=["POST"])
 def handle_prompt():
     data = Box(request.get_json())
-    
+
+    if not data.email or not data.query or data.category or data.image_base64:
+        return jsonify({
+        "message": "Successfully executed.",
+        "response": recommendation
+    }), 400
+
+    memory = get_memory_for_user(data.email)
+
     recommendation = rag_pipeline(data.query, data.category, data.image_base64, memory)
 
-
-
-    return jsonify({"message": "Successfully executed.","response":recommendation}), 200
+    return jsonify({
+        "message": "Successfully executed.",
+        "response": recommendation
+    }), 200
